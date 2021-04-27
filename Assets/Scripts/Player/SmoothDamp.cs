4c9a5255ba62f9ebe1f9fca8d3ca244ea5400a88
agdodson <andy.dodson@outlook.com>
2021-04-27 12:04:17 +0300

    - FPS character added.
    -- FPS view.
    -- Animation IK rigging, for FPS view.
    -- Custome reload animation.

---
+++ Assets/Scripts/Player/SmoothDamp.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDamp : MonoBehaviour
+{
+	[SerializeField] private Transform _target;
+	[SerializeField] private float _speed = 10.0f;
+
+	private void LateUpdate()
+	{
+		Vector3 targetPosition = _target.transform.position;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _speed);
+		transform.rotation = Quaternion.Euler(_target.transform.rotation.eulerAngles);
+	}
+}
